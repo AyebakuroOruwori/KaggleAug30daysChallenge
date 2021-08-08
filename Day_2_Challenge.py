f8e45fbfe850444418d9cc573ba424fa1bd33786
Ayebakuro A. Oruwori <ayebakurooruwori@gmail.com>
2021-08-08 02:45:08 +0100

    Create Day_2_Challenge.py

---
+++ Day_2_Challenge.py
@@ -0,0 +1,21 @@
+      #cell one
+#ruound_to_two_places(9.9999)
+
+round(9.9999, 2)
+
+
+      #cell two
+      
+x = -10
+y = 5
+# Which of the two variables above has the smallest absolute value?
+smallest_abs = min(abs(x), abs(y))
+print(smallest_abs)
+
+
+      #cell three
+      
+def f(x):
+        y = abs(x)
+        return y
+print(f(5))
