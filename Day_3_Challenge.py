ab3fcea91e4a1014af485a94718a4c7ff07c25b6
Ayebakuro A. Oruwori <ayebakurooruwori@gmail.com>
2021-08-08 13:26:29 +0100

    Update Day_3_Challenge.py

--- Day_3_Challenge.py
+++ Day_3_Challenge.py
@@ -12,3 +12,75 @@ def sign(num):
         return 0
 # Check your answer
 q1.check()
+
+
+#We've decided to add "logging" to our to_smash function from the previous exercise.
+def to_smash(total_candies ):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
+    
+    >>> to_smash(91)
+    1
+    """
+    print("Splitting", total_candies, "candies")
+    return total_candies % 3
+
+to_smash(91)
+
+
+#What happens if we call it with total_candies = 1?
+to_smash(1)
+
+
+#That isn't great grammar!
+
+#Modify the definition in the cell below to correct the grammar of our print statement. (If there's only one candy, we should use the singular "candy" instead of the plural "candies")
+
+def to_smash(total_candies):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
+    
+    >>> to_smash(91)
+    1
+    """
+    if total_candies ==1:
+        print("Only 1 candy")
+    else:
+        print("Splitting", total_candies, "candies")
+    return total_candies % 3
+
+to_smash(91)
+to_smash(1)
+
+""" 
+In the tutorial, we talked about deciding whether we're prepared for the weather. I said that I'm safe from today's weather if...
+
+I have an umbrella...
+or if the rain isn't too heavy and I have a hood...
+otherwise, I'm still fine unless it's raining and it's a workday
+The function below uses our first attempt at turning this logic into a Python expression. I claimed that there was a bug in that code. Can you find it?
+
+To prove that prepared_for_weather is buggy, come up with a set of inputs where either:
+
+the function returns False (but should have returned True), or
+the function returned True (but should have returned False).
+To get credit for completing this question, your code should return a Correct result.
+"""
+
+def prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday):
+    # Don't change this code. Our goal is just to find the bug, not fix it!
+    return have_umbrella or rain_level < 5 and have_hood or not rain_level > 0 and is_workday
+
+# Change the values of these inputs so they represent a case where prepared_for_weather
+# returns the wrong answer.
+have_umbrella = False
+rain_level = -0.5
+have_hood = False
+is_workday = False
+
+# Check what the function returns given the current values of the variables above
+actual = prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday)
+print(actual)
+
+# Check your answer
+q3.check()
